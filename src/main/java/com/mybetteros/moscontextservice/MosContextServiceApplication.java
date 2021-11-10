c7d6e7642671a6dc086f98d4b770e652f6de79d3
David Chan <mybetteros@outlook.com>
2021-11-09 20:48:37 -0800

    Initial setup of an empty spring boot project.

---
+++ src/main/java/com/mybetteros/moscontextservice/MosContextServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mybetteros.moscontextservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosContextServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosContextServiceApplication.class, args);
+	}
+
+}
